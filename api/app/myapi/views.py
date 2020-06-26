21fee30159c243d970122bf3438731971555e18e
Luis Arce <luis.arce22@gmail.com>
2020-06-26 19:25:47 -0400

    fix views.py

--- api/app/myapi/views.py
+++ api/app/myapi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .serializers import ProjectSerializer
+from .serializers import TaskSerializer
 
 from .models import Task
 from rest_framework.permissions import IsAuthenticated
