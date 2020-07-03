fe093d546c00b90658a3d38b4c702a3c1a05b90d
Luis Arce <luis.arce22@gmail.com>
2020-07-03 13:46:46 -0400

    integrate with jwt auth

--- api/app/app/settings.py
+++ api/app/app/settings.py
@@ -72,10 +72,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 # Database
