1400363c12bb1e3bd4ba3e50dc5bd85ea9070b4b
Luis Arce <luis.arce22@gmail.com>
2020-07-14 09:19:34 -0400

    update reset password comp

--- ui/src/app/client/reset-password/reset-password.component.ts
+++ ui/src/app/client/reset-password/reset-password.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
-
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ])
   constructor() { }
 
   ngOnInit(): void {
